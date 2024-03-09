69515ad446591e1dbfd9651f6eaa305d7dfc8ab4
Ethan Havemann <echavemann@gmail.com>
2024-03-08 19:43:31 -0600

    Remove initial functionality

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-    "fmt"
 
     "github.com/spf13/cobra"
 )
@@ -12,21 +11,12 @@ var rootCmd = &cobra.Command{
     Use: "cpg",
     Short: "Competitive Programming Utility",
     Run: func(cmd *cobra.Command, args[] string) {
-        source, _ := cmd.Flags().GetString("source")
-        destination, _ := cmd.Flags().GetString("destination")
-
-        if err := copyFileOrDir(source, destination); err != nil {
-            fmt.Printf("Error")
-        }
     },
 }
 
 func init() {
-	rootCmd.Flags().StringP("source", "s", "", "Source file or directory path")
-	rootCmd.Flags().StringP("destination", "d", "", "Destination directory path")
+	rootCmd.Flags().StringP("include", "i", "", "CPLib Packages to Include")
 
-	rootCmd.MarkFlagRequired("source")
-	rootCmd.MarkFlagRequired("destination")
 }
 
 func Execute() error {
